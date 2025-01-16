8d2a2df3aaa8a95b2ffb9c602de5c6d72af459ad
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2025-01-16 22:32:41 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0", // Mengizinkan akses dari luar localhost
-    port: 5173, // Port default (ubah jika diperlukan)
-    strictPort: true, // Pastikan Vite gagal jika port tidak tersedia
-    https: true, // Set ke true jika menggunakan HTTPS
-  },
 });
