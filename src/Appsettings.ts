d5bac15f15a11a14b54595472f200801b533edeb
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2025-01-29 21:39:13 +0700

    server

--- src/Appsettings.ts
+++ src/Appsettings.ts
@@ -1,12 +1,12 @@
 const apis = {
-  apiProd: "https://apibki.hcorp.my.id",
+  apiProd: "http://apibki.hcorp.my.id",
   apiDev: "http://localhost:4000",
   apiExpo: "http://192.168.253.14:4000",
   apiUndip: "http://192.168.10.110:4000",
 };
 
 const appSettings = {
-  api: apis.apiDev,
+  api: apis.apiProd,
 };
 
 export default appSettings;
