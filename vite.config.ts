25cc8554feaec07266038eed2eb28fb814cb6550
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2025-01-29 22:54:37 +0700

    server

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Pastikan server mendengarkan di semua network interfaces
+    hmr: {
+      protocol: "wss", // Gunakan wss untuk koneksi WebSocket di HTTPS
+      host: "bki.hcorp.my.id", // Host domain Anda
+      port: 443,
+    },
+  },
 });
