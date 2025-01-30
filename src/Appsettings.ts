a0f07b89eeb527e72fe55d5a07ee23d9fd58ef19
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2025-01-30 10:59:39 +0700

    server

--- src/Appsettings.ts
+++ src/Appsettings.ts
@@ -1,5 +1,5 @@
 const apis = {
-  apiProd: "https://apibki.hcorp.my.id",
+  apiProd: "http://apibki.hcorp.my.id",
   apiDev: "http://localhost:4000",
   apiExpo: "http://192.168.253.14:4000",
   apiUndip: "http://192.168.10.110:4000",
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/auth": {
+        target: "http://apibki.hcorp.my.id", // Ganti dengan alamat backend HTTP yang sesuai
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/auth/, ""), // Menghapus /auth di awal URL saat diteruskan ke backend
+      },
+    },
+  },
 });
