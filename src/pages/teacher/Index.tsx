b5cb40f24a631061221cd01c63090bf33914c003
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2025-03-01 15:49:07 +0700

    recap

--- src/pages/teacher/Index.tsx
+++ src/pages/teacher/Index.tsx
@@ -1,38 +1,39 @@
 import { useState, useContext, createContext } from "react";
-import Footer from "../../components/Footer"
-import Navbar from "./components/NavbarGuru"
-import { Outlet } from 'react-router-dom'
+import Footer from "../../components/Footer";
+import Navbar from "./components/NavbarGuru";
+import { Outlet } from "react-router-dom";
 import { AppContext } from "../../AppContext";
-import jwt from 'jwt-decode';
+import jwt from "jwt-decode";
 import Sidebar from "./components/Sidebar";
 import BypassPresensidanPerizinanMobile from "./BypassPresensidanPerizinanMobile";
 
-export const userContext = createContext({})
+export const userContext = createContext({});
 
 function Teacher() {
-    const token = useContext(AppContext).token.data;
-    let user = '';
-    try {
-        user = jwt(token)
-    } catch (err) { }
+  const token = useContext(AppContext).token.data;
 
-    const [userData, setUserData] = useState(user);
+  let user = "";
+  try {
+    user = jwt(token);
+  } catch (err) {}
 
-    return (
-        <userContext.Provider value={userData}>
-            <div className="flex flex-col items-center justify-start relative text-lg">
-                <Navbar className="bg-themeTeal" />
-                <div className="items-start w-full min-h-[100svh] hidden md:flex">
-                    <Sidebar />
-                    <Outlet />
-                </div>
-                <div className="md:hidden py-24">
-                    <BypassPresensidanPerizinanMobile />
-                </div>
-                <Footer />
-            </div>
-        </userContext.Provider>
-    );
+  const [userData, setUserData] = useState(user);
+
+  return (
+    <userContext.Provider value={userData}>
+      <div className="flex flex-col items-center justify-start relative text-lg">
+        <Navbar className="bg-themeTeal" />
+        <div className="items-start w-full min-h-[100svh] hidden md:flex">
+          <Sidebar />
+          <Outlet />
+        </div>
+        <div className="md:hidden py-24">
+          <BypassPresensidanPerizinanMobile />
+        </div>
+        <Footer />
+      </div>
+    </userContext.Provider>
+  );
 }
 
 export default Teacher;
